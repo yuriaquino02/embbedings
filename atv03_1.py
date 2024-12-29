d709d702775af943f0f4eade37eb2ab2b8808114
yuriaquino02 <yuricavalacantia@hotmail.com>
2024-12-28 21:23:25 -0300

    alteracao nos comentarios

--- atv03_1.py
+++ atv03_1.py
@@ -10,10 +10,10 @@ embeddings = OllamaEmbeddings(model="mxbai-embed-large")
 # Gera o embedding para a frase
 phrase_embedding = embeddings.embed_query(phrase)
 
-# Mostra o tamanho do embedding
+# Mostra o tamanho do embedding, por padrão do modelo "mxbai-embed-large" esse vetor terá 1024 valores
 embedding_size = len(phrase_embedding)
 print(f"Tamanho do embedding: {embedding_size}")
 
-# Exibe o vetor de embedding
+# Exibe os 10 primeiros valores do vetor de embedding 
 print(f"\nFrase: {phrase}")
 print(f"Vetor de Embedding: {phrase_embedding[:10]}\n")
